2e84723cc689c355cde9cc5961e40c8f130992e2
Gusev Maxim <gusev.nero@gmail.com>
2018-01-31 12:40:54 +0300

    -

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,15 +3,14 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-gulp:app', () => {
+describe('generator-gulp-i:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({ someAnswer: true });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['dummyfile.txt']);
   });
 });
